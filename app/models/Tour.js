a028456c24a725f920dc5c6ac3d6f42aa69eddba
VANMAK <david.vanmak@pop-france.eu>
2022-08-14 17:12:07 +0200

    [ADD] Add Advanced validation

--- app/models/Tour.js
+++ app/models/Tour.js
@@ -1,108 +1,128 @@
 import mongoose from 'mongoose';
-import { slugify } from '../helper/slugify';
-
+import { slugify } from '../helper/slugify.js';
+import validator from 'validator';
 /**
  * Tour's schema model for the Database
  * At registration: pseudo,email & password are required
  */
 const tourSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'A tour must have a name'],
-            unique: true,
-            trim: true,
-            maxlength: [
-                40,
-                'A tour name must have less or equal then 40 characters',
-            ],
-            minlength: [
-                10,
-                'A tour name must have more or equal then 10 characters',
-            ],
-            // validate: [validator.isAlpha, 'Tour name must only contain characters']
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour name is a required field'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less than 40 characters'],
+      minlength: [10, 'A tour name must be at least 10 characters'],
+      // validate: [validator.isAlpha, ''],
+      validate: {
+        validator: function (val) {
+          let regStringAndSpaces = /^[a-zA-Z\s]*$/;
+          return regStringAndSpaces.test(val);
         },
-        slug: String,
-        duration: {
-            type: Number,
-            required: [true, 'A tour must have a duration'],
-        },
-        maxGroupSize: {
-            type: Number,
-            required: [true, 'A tour must have a group size'],
-        },
-        difficulty: {
-            type: String,
-            required: [true, 'A tour must have a difficulty'],
-            enum: {
-                values: ['easy', 'medium', 'difficult'],
-                message: 'Difficulty is either: easy, medium, difficult',
-            },
-        },
-        ratingsAverage: {
-            type: Number,
-            default: 4.5,
-            min: [1, 'Rating must be above 1.0'],
-            max: [5, 'Rating must be below 5.0'],
-        },
-        ratingsQuantity: {
-            type: Number,
-            default: 0,
-        },
-        price: {
-            type: Number,
-            required: [true, 'A tour must have a price'],
-        },
-        // priceDiscount: {
-        //   type: Number,
-        //   validate: {
-        //     validator: function (val) {
-        //       // this only points to current doc on NEW document creation
-        //       return val < this.price;
-        //     },
-        //     message: 'Discount price ({VALUE}) should be below regular price',
-        //   },
-        // },
-        summary: {
-            type: String,
-            trim: true,
-            required: [true, 'A tour must have a description'],
-        },
-        description: {
-            type: String,
-            trim: true,
-        },
-        imageCover: {
-            type: String,
-            required: [true, 'A tour must have a cover image'],
-        },
-        images: [String],
-        createdAt: {
-            type: Date,
-            default: Date.now(),
-            select: false,
-        },
-        startDates: [Date],
-        secretTour: {
-            type: Boolean,
-            default: false,
-        },
+        message: 'Tour name must only contain characters',
+      },
     },
-    // ENABLE VIRTUAL POPULATE
-    {
-        toJSON: {virtuals: true},
-        toObject: {virtuals: true},
+
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
     },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a max group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty should be: easy, medium or difficuty',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be greater than 1'],
+      max: [5, 'Rating must less than 5'],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price; // only works for .create
+        },
+        message: 'Discount price should be less than regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      //required: [true, 'A tour must have a cover image']
+    },
+    image1: String,
+    image2: String,
+    image3: String,
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      type: String,
+    },
+    locations: [
+      {
+        type: String,
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    featured: Boolean,
+    guides: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 // VIRTUAL POPULATE
-tourSchema.virtual('durationWeeks').get(function(){
-  return this.duration/7
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
-  this.slug = slugify(this.name,"-");
+  this.slug = slugify(this.name, '-');
   next();
 });
 
@@ -111,19 +131,19 @@ tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
   next();
-})
+});
 
 // QUERY MIDDLEWARE - secret tours
 tourSchema.post(/^find/, function (_docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
-})
+});
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
-})
+});
 
 const Tour = mongoose.model('tour', tourSchema);
 
